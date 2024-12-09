d75e524ecca32ec6d3f7c2be1825875bd9908829
aketsu <anchutin09@bk.ru>
2024-12-09 19:12:49 +0300

    приложение для рисования прямоугльников

--- pg091224/main.py
+++ pg091224/main.py
@@ -14,24 +14,51 @@ if __name__ == '__main__':
     MYEVENTTYPE = pygame.USEREVENT + 1
     pygame.time.set_timer(MYEVENTTYPE, 1000)
 
+    # while running:
+    #     screen.fill("purple")
+    #     # poll for events
+    #     # pygame.QUIT event means the user clicked X to close your window
+    #     for event in pygame.event.get():
+    #         if event.type == pygame.QUIT:
+    #             running = False
+    #         if event.type == pygame.MOUSEMOTION:
+    #             pygame.draw.circle(screen, "red", event.pos, 40)
+    #             pygame.draw.circle(screen, "red", (event.pos[0], width - event.pos[1]), 40)
+    #             pygame.draw.circle(screen, "red", (height - event.pos[0], event.pos[1]), 40)
+    #         if event.type == MYEVENTTYPE:
+    #             print('Hello')
+    screen2 = pygame.Surface(screen.get_size())
+    x1, y1, w, h = 0, 0, 0, 0
+    drawing = False  # режим рисования выключен
     while running:
-        screen.fill("purple")
-        # poll for events
-        # pygame.QUIT event means the user clicked X to close your window
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                drawing = True  # включаем режим рисования
+                # запоминаем координаты одного угла
+                x1, y1 = event.pos
+            if event.type == pygame.MOUSEBUTTONUP:
+                # сохраняем нарисованное (на втором холсте)
+                screen2.blit(screen, (0, 0))
+                drawing = False
+                x1, y1, w, h = 0, 0, 0, 0
             if event.type == pygame.MOUSEMOTION:
-                pygame.draw.circle(screen, "red", event.pos, 40)
-                pygame.draw.circle(screen, "red", (event.pos[0], width - event.pos[1]), 40)
-                pygame.draw.circle(screen, "red", (height - event.pos[0], event.pos[1]), 40)
-            if event.type == MYEVENTTYPE:
-                print('Hello')
+                # запоминаем текущие размеры
+                if drawing:
+                    w, h = event.pos[0] - x1, event.pos[1] - y1
+        # рисуем на экране сохранённое на втором холсте
+        screen.fill(pygame.Color('black'))
+        screen.blit(screen2, (0, 0))
+        if drawing:  # и, если надо, текущий прямоугольник
+            if w > 0 and h > 0:
+                pygame.draw.rect(screen, (0, 0, 255), ((x1, y1), (w, h)), 5)
+            if w < 0 and h < 0:
+                pygame.draw.rect(screen, (0, 0, 255), ((x1 + w, y1 + h), (abs(w), abs(h))), 5)
+        pygame.display.flip()
         # fill the screen with a color to wipe away anything from last frame
 
         # flip() the display to put your work on screen
-        pygame.display.flip()
-
         # limits FPS to 60
         # dt is delta time in seconds since last frame, used for framerate-
         # independent physics.
